1c1118506c377ba3852a575c4af24c0860e282ab
howkeye17 <howkeye17@gmail.com>
2021-06-07 20:12:34 +0300

    Design for NewPlaceController Work with Image Alert Actions (+customisation)

---
+++ MyPlaces/PlaceModel.swift
@@ -0,0 +1,33 @@
+//
+//  PlaceModel.swift
+//  MyPlaces
+//
+//  Created by Valera Vasilevich on 7.06.21.
+//
+
+import Foundation
+
+struct Place {
+    
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+    
+    static let restaurantNames = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+        "Индокитай", "X.O","Балкан Гриль", "Sherlock Holmes",
+        "Speak Easy", "Morris Pub", "Вкусные истории",
+        "Классик", "Love&Life", "Шок", "Бочка"
+    ]
+    
+    static func getPlaces() -> [Place] {
+        
+        var places = [Place]()
+        for place in restaurantNames {
+            places.append(Place(name: place, location: "Уфа", type: "Ресторан", image: place))
+        }
+        
+        return places
+    }
+}
