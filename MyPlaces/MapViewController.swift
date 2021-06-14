8e59f0519b803b97899cf7e70b1abfc22a50eedd
howkeye17 <howkeye17@gmail.com>
2021-06-14 14:29:20 +0300

    Add user Location Work with authorizationSatus in CoreLocation (mapView) Button for center ViewInUserLocation

--- MyPlaces/MapViewController.swift
+++ MyPlaces/MapViewController.swift
@@ -7,11 +7,14 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
-
+    
     var place = Place()
     let annotationIdentifier = "annotationIdentifier"
+    let locationManager = CLLocationManager()
+    let regionInMeters = 10_000.00
     
     @IBOutlet weak var mapView: MKMapView!
     
@@ -20,10 +23,19 @@ class MapViewController: UIViewController {
         
         mapView.delegate = self
         setupPlacemark()
-
+        checkLocationServices()
+        
     }
     
     
+    @IBAction func centerViewInUserLocation() {
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion(center: location,
+                                            latitudinalMeters: regionInMeters,
+                                            longitudinalMeters: regionInMeters)
+            mapView.setRegion(region, animated: true)
+        }
+    }
     
     @IBAction func closeVC() {
         dismiss(animated: true)
@@ -55,7 +67,88 @@ class MapViewController: UIViewController {
             self.mapView.selectAnnotation(annotation, animated: true)
         }
     }
-
+    
+    private func checkLocationServices() {
+        
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocationManager()
+            locationManagerDidChangeAuthorization(locationManager)
+            // checkLocationAutorization()
+        } else {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.showAlert(
+                    title: "Location Services are Disabled",
+                    message: "To enable it go: Settings -> Privacy -> Location Services and turn On")
+            }
+        }
+    }
+    
+    private func setupLocationManager() {
+        //  locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    private func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) { // вместо checkLocationAutorization!
+        switch manager.authorizationStatus {
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            break
+        case . denied:
+            // show alert controller
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.showAlert(
+                    title: "Location Services are Disabled",
+                    message: "To enable it go: Settings -> Privacy -> Location Services and turn On") }
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            break
+        case .restricted:
+            // show alert controller
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self.showAlert(
+                    title: "Location Services are Disabled",
+                    message: "To enable it go: Settings -> Privacy -> Location Services and turn On") }
+            break
+        case .authorizedAlways:
+            break
+        @unknown default:
+            print ("New case is availible")
+        }
+    }
+    
+    private func showAlert (title: String, message: String) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
+    /*
+     private func checkLocationAutorization() {
+     switch CLLocationManager.authorizationStatus() {  -- устарело после iOS 14 и новее
+     case .authorizedWhenInUse:
+     mapView.showsUserLocation = true
+     break
+     case . denied:
+     // show alert controller
+     break
+     case .notDetermined:
+     locationManager.requestWhenInUseAuthorization()
+     break
+     case .restricted:
+     // show alert controller
+     break
+     case .authorizedAlways:
+     break
+     @unknown default:
+     print ("New case is availible")
+     }
+     }
+     */
+    
 }
 
 extension MapViewController: MKMapViewDelegate {
@@ -83,3 +176,10 @@ extension MapViewController: MKMapViewDelegate {
         return annotationView
     }
 }
+
+//extension MapViewController: CLLocationManagerDelegate {
+//
+//    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+//        setupLocationManager()
+//    }
+//}
